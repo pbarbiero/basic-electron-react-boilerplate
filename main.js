41d1c2bd9259976d9c464a4d8c6ac9b8bdb6d8ed
TheD <gianszabernardo@gmail.com>
2018-06-03 18:20:03 -0300

    Change function to arrow function

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function createWindow() {
   });
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
